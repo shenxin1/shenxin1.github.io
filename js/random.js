5792d1948f92d8399b9e629428f008596bd84ab3
shenxin1 <210436263@qq.com>
2023-04-05 14:57:57 +0800

    Site updated: 2023-04-05 14:57:56

--- js/random.js
+++ js/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/04/04/一篇博客/","2023/04/05/HTML学习/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
+var posts=["2023/04/05/HTML学习/","2023/04/04/一篇博客/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
