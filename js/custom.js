4461e233fb8130ee0c2595d9cf47e637b99138e6
shenxin1 <210436263@qq.com>
2023-04-06 20:16:25 +0800

    Site updated: 2023-04-06 20:16:24

--- js/custom.js
+++ js/custom.js
@@ -3,10 +3,10 @@ var OriginTitile = document.title;    // 保存之前页面标题
 var titleTime;
 document.addEventListener('visibilitychange', function(){
     if (document.hidden){
-        document.title = '404 Not Found';
+        document.title = '找不到页面';
         clearTimeout(titleTime);
     }else{
-        document.title = '上当了吧哈哈！';
+        document.title = '又回来啦';
         titleTime = setTimeout(function() {
             document.title = OriginTitile;
         }, 3000); // 3秒后恢复原标题
--- js/random.js
+++ js/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/04/05/HTML学习/","2023/04/05/CSS学习/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
+var posts=["2023/04/05/CSS学习/","2023/04/05/HTML学习/","2023/04/06/README/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
